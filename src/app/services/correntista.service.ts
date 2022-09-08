cf8a6098f1c40d2ff4135e106eb21fb129417456
pedro-negrao <pedro.negrao12@gmail.com>
2022-09-08 19:55:21 -0300

    Mudança da url em prod

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const baseUrl = 'http://localhost:8080';
+//const baseUrl = 'http://localhost:8080';
+const baseUrl= environment.API_URL;
 
 @Injectable({
     providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://negraop-bankline-api.herokuapp.com',
 };
